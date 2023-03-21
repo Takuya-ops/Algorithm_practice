c33661a485a70d2a8cfd778462dea4c467ef3d49
Takuya-ops <hora2112.ipu@gmail.com>
2023-03-21 12:00:18 +0900

    daily_coding

---
+++ 2348. Number of Zero-Filled Subarrays.py
@@ -0,0 +1,12 @@
+class Solution:
+    def zeroFilledSubarray(self, nums: List[int]) -> int:
+        ans, num_subarray = 0, 0
+
+        for num in nums:
+            if num == 0:
+                num_subarray += 1
+            else:
+                num_subarray = 0
+            ans += num_subarray
+
+        return ans
