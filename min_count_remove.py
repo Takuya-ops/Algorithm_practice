19710c2a82e51ff0e98e483d168fb7c3af7611a5
Takuya-ops <hora2112.ipu@gmail.com>
2023-04-29 20:26:52 +0900

    min_count_remove

---
+++ min_count_remove.py
@@ -0,0 +1,33 @@
+from typing import List
+from collections import Counter
+
+
+def min_count_remove(x: List[int], y: List[int]) -> None:
+    # count_x = {}
+    # count_y = {}
+    # for i in x:
+    #     count_x[i] = count_x.get(i, 0) + 1
+    # for i in y:
+    #     count_y[i] = count_y.get(i, 0) + 1
+
+    # counterを使うことで、簡単に実装できる
+    counter_x = Counter(x)
+    counter_y = Counter(y)
+
+    for key_x, value_x in counter_x.items():
+        value_y = counter_y.get(key_x)
+        if value_y:
+            if value_x < value_y:
+                x[:] = [i for i in x if i != key_x]
+            elif value_x > value_y:
+                y[:] = [i for i in y if i != key_x]
+
+    # 辞書の中からキーとバリューの値を取り出す
+if __name__ == "__main__":
+    x = [1, 2, 3, 4, 4, 5, 5, 8, 10]
+    y = [4, 5, 5, 5, 6, 7, 8, 8, 10]
+    print("x = ", x)
+    print("y = ", y)
+    min_count_remove(x, y)
+    print("x = ", x)
+    print("y = ", y)
