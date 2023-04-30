b7909e2ff0d98663ed450ea797b7ddfee6efafd4
Takuya-ops <hora2112.ipu@gmail.com>
2023-04-30 17:42:17 +0900

    daily_coding

--- bubble_sort2.py
+++ bubble_sort2.py
@@ -1,6 +1,8 @@
 from typing import List
 
-def bubble_sort(numbers:List[int]) -> int
+
+def bubble_sort(numbers: List[int]) -> int
+
 
 if __name__ == "__main__":
-list1 = [1,2,3]
+list1 = [1, 2, 3]
--- cache_func.py
+++ cache_func.py
@@ -5,23 +5,28 @@ import time
 # 自作のキャッシュ用のデコレータ
 # デコレータは関数やクラスの前後に特定の処理を追加できる機能
 
+# キャッシュに使用するためのデコレータの作成（lru_cacheの中身）
+
 
 def memoize(f):
     # 辞書型で格納しておく
     cache = {}
 
     def _wrapper(n):
+        # cacheの中に含まれていない場合、辞書の中に追加する
         if n not in cache:
             cache[n] = f(n)
         return cache[n]
     return _wrapper
+
+# 作成したデコレータ関数の使用
 
 
 @memoize
 # @lru_cache()
 def long_func(num: int) -> int:
     r = 0
-    for i in range(10000000):
+    for i in range(20000000):
         r += num * i
     return r
 
