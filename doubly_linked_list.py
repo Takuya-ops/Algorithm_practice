7ae8dd9bde2bd91dd4153d40c7ee4dd92c322e91
Takuya-ops <hora2112.ipu@gmail.com>
2023-06-05 04:12:53 +0900

    sort doubly linked list

--- doubly_linked_list.py
+++ doubly_linked_list.py
@@ -103,13 +103,29 @@ class DoublyLinkedList(object):
 
         self.head = _reverse_recursive(self.head)
 
+    def sort(self) -> None:
+        if self.head is None:
+            return
+
+        current_node = self.head
+        while current_node.next:
+            next_node = current_node.next
+            while next_node:
+                if current_node.data > next_node.data:
+                    current_node.data, next_node.data = (
+                        next_node.data,
+                        current_node.data,
+                    )
+                next_node = next_node.next
+            current_node = current_node.next
+
 
 if __name__ == "__main__":
     d = DoublyLinkedList()
     d.append(0)
+    d.append(5)
     d.append(1)
-    d.append(2)
-    d.append(3)
+    d.append(9)
     # d.insert(0)
     d.print()
     # print(d.head.data)
@@ -124,4 +140,7 @@ if __name__ == "__main__":
     d.print()
     print("########### Reverse Rec")
     d.reverse_recursive()
+    d.print()
+    print("########### Sort")
+    d.sort()
     d.print()
