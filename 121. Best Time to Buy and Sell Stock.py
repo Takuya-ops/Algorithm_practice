f7c46d3684223d778ef20f2b9e71e91b98674f23
Takuya-ops <hora2112.ipu@gmail.com>
2023-06-07 09:42:16 +0900

    daily_coding

--- 121. Best Time to Buy and Sell Stock.py
+++ 121. Best Time to Buy and Sell Stock.py
@@ -1,6 +1,6 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        min_price = float('inf')
+        min_price = float("inf")
         max_profit = 0
         for i in range(len(prices)):
             if prices[i] < min_price:
