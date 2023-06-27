e26d36b06a2b949cc9652d116f8b5bf05a100641
Takuya-ops <hora2112.ipu@gmail.com>
2023-06-27 20:50:34 +0900

    daily_coding

--- 373. Find K Pairs with Smallest Sums.py
+++ 373. Find K Pairs with Smallest Sums.py
@@ -11,7 +11,7 @@ class Solution:
         visited = set()
 
         minHeap = [(nums1[0] + nums2[0], (0, 0))]
-        visited.add((0.0, 0))
+        visited.add((0, 0))
         count = 0
 
         while k > 0 and minHeap:
@@ -20,8 +20,11 @@ class Solution:
 
             if i + 1 < m and (i + 1, j) not in visited:
                 heappush(minHeap, (nums1[i + 1] + nums2[j], (i + 1, j)))
+                visited.add((i + 1, j))
+
             if j + 1 < n and (i, j + 1) not in visited:
                 heappush(minHeap, (nums1[i] + nums2[j + 1], (i, j + 1)))
                 visited.add((i, j + 1))
             k = k - 1
+
         return ans
