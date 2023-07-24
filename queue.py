6ae3806b505b26218c7b6a50e97041afbd9eaf39
Takuya-ops <hora2112.ipu@gmail.com>
2023-07-24 23:26:17 +0900

    queue

---
+++ queue.py
@@ -0,0 +1,25 @@
+from typing import Any
+
+
+class Stack:
+    def __init__(self) -> None:
+        self.stack = []
+
+    def push(self, data) -> None:
+        self.stack.append(data)
+
+    def pop(self) -> Any:
+        if self.stack:
+            return self.stack.pop()
+
+
+if __name__ == "__main__":
+    stack = Stack()
+    # 空のリスト
+    print(stack.stack)
+    stack.push(1)
+    print(stack.stack)
+    stack.push(2)
+    print(stack.stack)
+    stack.pop()
+    print(stack.stack)
