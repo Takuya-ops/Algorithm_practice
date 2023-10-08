09590d8f1016b3c8262f17d4b979f4c279020c26
Takuya-ops <hora2112.ipu@gmail.com>
2023-10-08 13:55:24 +0900

    daily_coding

---
+++ getPermutations.py
@@ -0,0 +1,11 @@
+def getPermutations(array):
+    def helper(array):
+        if len(array) == 1:
+            yield array
+
+        for idx, element in enumerate(array):
+            remaining = array[:idx] + array[idx + 1 :]
+            for sub_perm in helper(remaining):  # "remainig" ではなく "remaining" を使います
+                yield [element] + sub_perm
+
+    return list(helper(array))
