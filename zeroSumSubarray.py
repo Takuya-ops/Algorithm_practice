fa9403e30986ef55029dec075b7bc5be9f3be133
Takuya-ops <hora2112.ipu@gmail.com>
2023-12-15 01:42:49 +0900

    test

--- zeroSumSubarray.py
+++ zeroSumSubarray.py
@@ -7,14 +7,3 @@ def zeroSumSubarray(nums):
             if cumSum == 0:
                 return True
     return False
-
-
-def zeroSumSubarray(nums):
-    for i in range(len(nums)):
-        cumSum = 0
-        for j in range(i, len(nums)):
-            cumSum += nums[j]
-
-            if cumSum == 0:
-                return True
-    return False
