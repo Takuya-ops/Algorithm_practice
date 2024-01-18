521e1ac489c177dda3977021367f2958e90d9604
Takuya-ops <hora2112.ipu@gmail.com>
2024-01-18 18:27:17 +0900

    daily_coding

---
+++ climbStairs.py
@@ -0,0 +1,16 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        memo = [0] * (n + 1)
+        return self._climb_Stairs(0, n, memo)
+
+    def _climb_Stairs(self, i: int, n: int, memo: list) -> int:
+        if i > n:
+            return 0
+        if i == n:
+            return 1
+        if memo[i] > 0:
+            return memo[i]
+        memo[i] = self._climb_Stairs(i + 1, n, memo) + self._climb_Stairs(
+            i + 2, n, memo
+        )
+        return memo[i]
