07601e6191eb798accc37b776cbaef809fa3547f
Takuya-ops <hora2112.ipu@gmail.com>
2024-01-31 14:28:36 +0900

    daily_coding

---
+++ dailyTemperatures.py
@@ -0,0 +1,11 @@
+from typing import List
+
+
+class Solution:
+    def dailyTemperatures(self, T: List[int]) -> List[int]:
+        stack, r = [], [0] * len(T)
+        for i, t in enumerate(T):
+            while stack and T[stack[-1]] < t:
+                r[stack.pop()] = i - stack[-1]
+            stack.append(i)
+        return r
