36215974fa5759ea0b6ae8824a22b98d24e7e6f0
Takuya-ops <hora2112.ipu@gmail.com>
2024-02-02 13:51:01 +0900

    daily_coding

---
+++ sequentialDigits.py
@@ -0,0 +1,19 @@
+from typing import List
+
+
+class Solution:
+    def sequentialDigits(self, low: int, high: int) -> List[int]:
+        result = []
+
+        for i in range(1, 10):
+            num = i
+            next_digit = i + 1
+
+            while num <= high and next_digit <= 9:
+                num = num * 10 + next_digit
+                if low <= num <= high:
+                    result.append(num)
+                next_digit += 1
+
+        result.sort()
+        return result
